ea42b899fd3bad8f34fd79263327f6856f500ffb
miguelvalden <miguelvalden77@gmail.com>
2022-11-04 09:57:17 +0100

    clase 5

---
+++ javascript.js
@@ -0,0 +1,62 @@
+// 1. Haz una función que reciba como parámetro un número y devuelva ese número pero en negativo.
+    // Ejemplo: 1 -> -1 / 5.5 -> -5.5
+
+    const negative_transformer = ()=>{
+
+    }
+
+// 2. Haz una función que toma como parámetro un booleano y si es true devuelve "Yes", si es false devuelve "no"
+
+const bool = ()=>{
+
+}
+
+// 3. Haz un for desde 1 hasta 15 y cuando i sea multiplo de 3 se debe hacer un console.log() en el que ponga "Fizz" y cuando i sea multiplo de 5 se debe hacer un console.log() en el que ponga "Buzz", si es múltiplo de ambos se debe imprimir por pantalla "FizzBuzz"
+
+
+
+
+// 4. Caculadora: Haz una función calculadora que reciba tres parámetros, el primero se el primer número, el segundo es el símbolo y e tercero es el segundo número de la operación. Símbolos a incluir -> -, +, *, /.
+// Parámetros: 4, "-", 2 -> return: 2
+
+const calculadora = ()=>{
+    
+}
+
+// 5. Haz una función que saque el número de identidad de los usuarios de una aplicación. La url de la que tenemos que sacar el id está compuesta por varios datos pero hay que obtener los números del final.
+// Parámetro: https://aplication/users/id/287 -> return 287
+
+const get_id = ()=>{
+
+}
+
+
+// 6. Haz una función que reciba como parámetro un array y debe devolver del revés el array recibido como parámetro.
+
+const reverser_arr = ()=>{
+
+}
+
+// 7. Haz una función similar pero que devuelva del revés un string recibido como parámetro
+
+const reverser_string = ()=>{
+
+}
+
+// 8. Haz una función que calcule la media de un array de números que se pasará como parámetro
+
+const media_function = ()=>{
+
+}
+
+// 9. Teniendo en cuenta la estructura del objeto pasado por parámetro, Haz una función que salude a la persona por su nombre, apellido y le diga cual es su edad.
+
+const persona = {
+    name: "Jhon",
+    surname: "Davies",
+    age: 33
+}
+
+const greetings = ()=>{
+
+}
